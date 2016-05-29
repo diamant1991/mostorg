c1d0a7c6db123b068127da4bc5326ce23cf0d018
diamant1991 <diamant1988@mail.ru>
2016-05-29 15:37:03 +0300

    add files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,61 @@
-function opcl(i){if("none"==$(i).css("display")){for(var t in mydivs)$(mydivs[t]).hide();$(i).show()}}var mydivs=new Array("#recommend-hits","#new","#sale","#map","#scheme");$(".tab-item").click(function(i){$(".tab-item").removeClass("active"),$(this).addClass("active")}),$(".contacts__tab li").click(function(i){$(".contacts__tab li").removeClass("active"),$(this).addClass("active")}),$(".vacancy__header").click(function(i){var t=$(this).next();t.is(":hidden")?($(".vacancy__header").removeClass("active"),$(".vacancy__content").slideUp(350),t.slideDown(350),$(this).addClass("active")):(t.slideUp(350),$(this).removeClass("active"))}),$(window).scroll(function(){$(this).scrollTop()>200?$(".up-button").fadeIn():$(".up-button").fadeOut()}),$(".up-button").click(function(){return $("body,html").animate({scrollTop:0},800),!1}),$(".mobile-catalog__btn").click(function(i){i.preventDefault();var t=$(this).next();t.is(":hidden")?t.slideDown(300):t.slideUp(300)});
+var mydivs = new Array('#recommend-hits','#new','#sale', '#map','#scheme');
+
+function opcl(e){
+  if ($(e).css('display') == 'none'){
+      for(var i in mydivs){   
+         $(mydivs[i]).hide();     
+      }
+      $(e).show();       
+  }
+}
+
+$('.tab-item').click(function(e) {
+	$('.tab-item').removeClass('active');
+	$(this).addClass('active');
+});
+
+$('.contacts__tab li').click(function(e) {
+	$('.contacts__tab li').removeClass('active');
+	$(this).addClass('active');
+});
+
+$('.vacancy__header').click(function(e) {
+	var content = $(this).next();
+	if(content.is(':hidden')){
+		$('.vacancy__header').removeClass('active');
+		$('.vacancy__content').slideUp(350);
+		content.slideDown(350);
+		$(this).addClass('active');
+	}
+	else{
+		content.slideUp(350);
+		$(this).removeClass('active');
+	}
+});
+
+$ (window).scroll (function () {
+	if ($ (this).scrollTop () > 200) {
+		$ ('.up-button').fadeIn();
+	} 
+	else {
+		$ ('.up-button').fadeOut();
+	}
+});
+ 
+$('.up-button').click(function(){
+	$('body,html').animate({
+		scrollTop: 0
+	}, 800);
+	return false;
+});
+
+$('.mobile-catalog__btn').click(function(e) {
+	e.preventDefault();
+	var cat = $(this).next()
+	if(cat.is(':hidden')){
+		cat.slideDown(300)
+	}
+	else{
+		cat.slideUp(300)
+	}
+});
